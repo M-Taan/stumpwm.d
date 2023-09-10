d222d3d0cd3622b951436ffc29b617ebd546c9f5
Mohamad Taan <mohamadmtaan@gmail.com>
2023-09-10 20:21:07 +0300

    feat: added emacsclient and firefox keybindings

--- init.lisp
+++ init.lisp
@@ -3,6 +3,9 @@
 ;; Setting default prefix
 (set-prefix-key (kbd "C-z"))
 
+;; defaults
+(setf *startup-message* nil)
+
 ;;; Groups
 
 ;; The goal is to always find what I'm looking for
@@ -17,6 +20,28 @@
 (gnewbg "[TERM]")
 (gnewbg "[MISC]")
 (gnewbg "[SLACK]")
+
+;; Startup
+(run-shell-command "/usr/local/bin/emacs --daemon")
+(run-shell-command "nitrogen --restore")
+(run-shell-command "dunst")
+
+;; Binding
+;; Window classes are set by Xorg, you can get the window class through: (window-class window)
+;; or through xorg's: xprop WM_CLASS
+(defcommand emacsclient () ()
+            (run-or-raise "emacsclient -c" '(:class "Emacs")))
+
+(defcommand firefox () ()
+            (run-or-raise "firefox" '(:class "firefox")))
+
+(setf *applications-keymap*
+      (let ((m (make-sparse-keymap)))
+        (define-key m (kbd "b") "firefox")
+        m))
+
+(define-key *root-map* (kbd "a") '*applications-keymap*)
+(define-key *root-map* (kbd "e") "emacsclient")
 
 ;;; Modeline
 (load (concat (getenv "HOME") "/.stumpwm.d/widgets/memory.lisp"))
